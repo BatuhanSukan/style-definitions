59d8f8d97fb479dd6f80f538f961403d7d0b389a
BatuhanSukan <48777954+BatuhanSukan@users.noreply.github.com>
2022-05-13 13:55:27 +0300

    d

---
+++ src/components/A/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./styles.module.css";
+
+
+console.log("A",styles);
+
+function A() {
+  return (
+    <div className={styles.title}>A</div>
+  )
+  
+  
+}
+
+export default A
